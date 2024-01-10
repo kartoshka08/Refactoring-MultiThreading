c830914cd7a1ecdcff6dee313db77a435274b19f
Kartoshka <kartoshka0811@gmail.com>
2024-01-10 13:22:10 +0300

    Update

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int poolSizeThreads = 64;
+        int port = 9999;
+        Server server = new Server(port, poolSizeThreads);
+        server.start();
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,108 @@
+package org.example;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private int socketInt;
+    private final List validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
+            "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    private String path;
+    private ExecutorService executor;
+
+    public Server(int socketInt, int poolSizeThreads) {
+        this.socketInt = socketInt;
+        executor = Executors.newFixedThreadPool(poolSizeThreads);
+    }
+
+    public void proceedConnection(Socket socket) throws IOException {
+        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+
+        // read only request line for simplicity
+        // must be in form GET /path HTTP/1.1
+        final var requestLine = in.readLine();
+        final var parts = requestLine.split(" ");
+
+        if (parts.length != 3) {
+            // just close socket
+            socket.close();
+            return;
+        }
+
+        path = parts[1];
+        if (!validPaths.contains(path)) {
+            out.write((
+                    "HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Length: 0\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.flush();
+        }
+
+        final var filePath = Path.of(".", "public", path);
+        final var mimeType = Files.probeContentType(filePath);
+
+        // special case for classic
+        if (path.equals("/classic.html")) {
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+        }
+
+        final var length = Files.size(filePath);
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
+    }
+
+
+    public void start() {
+        try (final var serverSocket = new ServerSocket(socketInt)) {
+            while (!serverSocket.isClosed()) {
+                try (final var socket = serverSocket.accept()) {
+                    executor.execute(() -> {
+                        try {
+                            proceedConnection(socket);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
