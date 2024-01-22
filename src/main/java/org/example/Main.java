790c992893533285c7dc23f9e103e2374e53fc44
Kartoshka <kartoshka0811@gmail.com>
2024-01-22 21:46:39 +0300

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int poolSizeThreads = 64;
-        int port = 8888;
+        int port = 9999;
         Server server = new Server(port, poolSizeThreads);
         server.start();
     }
